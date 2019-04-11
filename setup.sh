a39d3db3d644114ae1d468b5c84dd65ae198cfc2
Florent Vilmart <florent@flovilmart.com>
2019-04-11 17:56:01 -0400

    fic

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 CN=local.server
 
+# append the CN
+sudo echo "127.0.0.1 $CN" >> /etc/hosts
+
 openssl req \
     -new \
     -newkey rsa:4096 \
@@ -9,5 +12,3 @@ openssl req \
     -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=$CN" \
     -keyout $CN.key \
     -out $CN.cert
-
-
